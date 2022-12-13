7c9d7e1b89ae033e246ea7c363efbe21aef36dc5
zalogarciam <gegarciam95@gmail.com>
2022-12-12 23:00:50 -0500

    Implement calculator in js

--- semana-6/calculadora/js/index.js
+++ semana-6/calculadora/js/index.js
@@ -1,7 +1,18 @@
 'use strict';
 
 const documentReady = () => {
-  alert('Gon');
+  const calculatorForm = document.getElementById('calculatorForm');
+
+  const calculate = (e) => {
+    e.preventDefault();
+    const number1 = document.getElementById('number1').value;
+    const operator = document.getElementById('operator').value;
+    const number2 = document.getElementById('number2').value;
+
+    alert(eval(+number1 + operator + +number2));
+  };
+
+  calculatorForm.addEventListener('submit', calculate);
 };
 
 document.addEventListener('DOMContentLoaded', documentReady);
