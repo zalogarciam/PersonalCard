4961e3b0e75ebb8529a796049ed318803ec85acd
zalogarciam <gegarciam95@gmail.com>
2022-12-13 22:24:03 -0500

    Updating logic of no button

--- semana-6/kierez-zer-mi-nobia/js/index.js
+++ semana-6/kierez-zer-mi-nobia/js/index.js
@@ -1,7 +1,15 @@
 'use strict';
 
 const documentReady = () => {
-    alert('Please wait');
+    const yesButton = document.getElementById('yesButton');
+    const noButton = document.getElementById('noButton');
+  
+    const brokenHeart = () => {
+      noButton.style.top = Math.random() * innerHeight + 'px';
+      noButton.style.left = Math.random() * innerWidth + 'px';
+    };
+  
+    noButton.addEventListener('mouseover', brokenHeart);
 };
 
 document.addEventListener('DOMContentLoaded' ,documentReady);
