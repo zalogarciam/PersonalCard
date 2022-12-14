5450c4b6e9c0f8da458b98c7e55d2f35ff0a55ea
zalogarciam <gegarciam95@gmail.com>
2022-12-13 22:37:43 -0500

    Update yes button

--- semana-6/kierez-zer-mi-nobia/js/index.js
+++ semana-6/kierez-zer-mi-nobia/js/index.js
@@ -8,8 +8,16 @@ const documentReady = () => {
       noButton.style.top = Math.random() * innerHeight + 'px';
       noButton.style.left = Math.random() * innerWidth + 'px';
     };
+
+    const loveHeart = () => { 
+        alert('❤️ NoW ZoMoZ PartNer ❤️');
+        alert('♥ U Me DaZ LuZ & EsPerrAnZa A My Laif ♥');
+        // alert('😱 OuR VoDa Is MaNaNa 😱');
+        location.href = 'https://www.youtube.com/watch?v=LV89uSSQUL0';
+    }
   
     noButton.addEventListener('mouseover', brokenHeart);
+    yesButton.addEventListener('click', loveHeart);
 };
 
 document.addEventListener('DOMContentLoaded' ,documentReady);
