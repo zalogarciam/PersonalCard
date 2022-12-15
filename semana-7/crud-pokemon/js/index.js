7468181ee1a0e0d6719c2b947d0aa6f671775cb4
zalogarciam <gegarciam95@gmail.com>
2022-12-14 23:00:14 -0500

    Adding array to store pokemons

--- semana-7/crud-pokemon/js/index.js
+++ semana-7/crud-pokemon/js/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const documentReady = () => {
+  const pokemons = [];
   const tBodyPokemons = document.getElementById("tBodyPokemons");
   const formPokemon = document.getElementById("formPokemon");
 
@@ -13,14 +14,17 @@ const documentReady = () => {
     const attack = documentFormPokemon.attack.value;
     const special = documentFormPokemon.special.value;
     const imgUrl = documentFormPokemon.imgUrl.value;
+    pokemons.push({ name, type, hp, attack, special, imgUrl });
     console.log({ name, type, hp, attack, special, imgUrl });
     readPokemons({ name, type, hp, attack, special, imgUrl });
   };
 
   const readPokemons = (pokemon) => {
-    const { name, type, hp, attack, special, imgUrl } = pokemon;
-    tBodyPokemons.innerHTML += `<tr>
-    <td> 1 </td>
+    tBodyPokemons.innerHTML = "";
+    pokemons.forEach((element, index) => {
+      const { name, type, hp, attack, special, imgUrl } = element;
+      tBodyPokemons.innerHTML += `<tr>
+    <td> ${index}  </td>
     <td> ${name} </td>
     <td>${type}</td>
     <td>${hp} </td>
@@ -31,6 +35,7 @@ const documentReady = () => {
     </td>
     <td> 🗑️ | ✏️</td>
 </tr>`;
+    });
   };
 
   formPokemon.addEventListener("submit", createPokemon);
