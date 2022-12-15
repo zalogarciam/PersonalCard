0e1d9bc05691aa6e7ef9a98598f30ce74cac3786
zalogarciam <gegarciam95@gmail.com>
2022-12-14 21:46:55 -0500

    Create pokemon submit

--- semana-7/crud-pokemon/js/index.js
+++ semana-7/crud-pokemon/js/index.js
@@ -1,22 +1,32 @@
-'use strict';
+"use strict";
 
 const documentReady = () => {
-  const tBodyPokemons = document.getElementById('tBodyPokemons');
+  const tBodyPokemons = document.getElementById("tBodyPokemons");
+  const formPokemon = document.getElementById("formPokemon");
+
   tBodyPokemons.innerHTML += `
-  <tr>
-                                        <td> 1 </td>
-                                        <td> Pikachu </td>
-                                        <td>Electrico</td>
-                                        <td>250 </td>
-                                        <td>Trueno</td>
-                                        <td>Impactrueno</td>
-                                        <td>
-                                            <img src="https://images.wikidexcdn.net/mwuploads/wikidex/thumb/7/77/latest/20150621181250/Pikachu.png/1200px-Pikachu.png"
-                                                alt="Pikachu" class="img-fluid" style="max-width: 128px;" />
-                                        </td>
-                                        <td> 🗑️ | ✏️</td>
-                                    </tr>
+
+    <tr>
+        <td> 1 </td>
+        <td> Pikachu </td>
+        <td>Electrico</td>
+        <td>250 </td>
+        <td>Trueno</td>
+        <td>Impactrueno</td>
+        <td>
+            <img src="https://images.wikidexcdn.net/mwuploads/wikidex/thumb/7/77/latest/20150621181250/Pikachu.png/1200px-Pikachu.png"
+                alt="Pikachu" class="img-fluid" style="max-width: 128px;" />
+        </td>
+        <td> 🗑️ | ✏️</td>
+    </tr>
   `;
+
+  const createPokemon = (e) => {
+    e.preventDefault();
+    console.log("Gon");
+  };
+
+  formPokemon.addEventListener("submit", createPokemon);
 };
 
-document.addEventListener('DOMContentLoaded', documentReady);
+document.addEventListener("DOMContentLoaded", documentReady);
