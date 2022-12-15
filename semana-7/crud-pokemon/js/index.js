bd2aeef55b5bd9e461cb2746578ff3d16860678e
zalogarciam <gegarciam95@gmail.com>
2022-12-14 21:38:26 -0500

    Add row in table programatically

--- semana-7/crud-pokemon/js/index.js
+++ semana-7/crud-pokemon/js/index.js
@@ -1,7 +1,22 @@
 'use strict';
 
 const documentReady = () => {
-  alert('XD')
+  const tBodyPokemons = document.getElementById('tBodyPokemons');
+  tBodyPokemons.innerHTML += `
+  <tr>
+                                        <td> 1 </td>
+                                        <td> Pikachu </td>
+                                        <td>Electrico</td>
+                                        <td>250 </td>
+                                        <td>Trueno</td>
+                                        <td>Impactrueno</td>
+                                        <td>
+                                            <img src="https://images.wikidexcdn.net/mwuploads/wikidex/thumb/7/77/latest/20150621181250/Pikachu.png/1200px-Pikachu.png"
+                                                alt="Pikachu" class="img-fluid" style="max-width: 128px;" />
+                                        </td>
+                                        <td> 🗑️ | ✏️</td>
+                                    </tr>
+  `;
 };
 
 document.addEventListener('DOMContentLoaded', documentReady);
