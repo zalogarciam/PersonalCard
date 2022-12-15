c837e98f0c808e504ae1828fdd763218d6aa3195
zalogarciam <gegarciam95@gmail.com>
2022-12-14 22:30:48 -0500

    Add fields for each column

--- semana-7/crud-pokemon/js/index.js
+++ semana-7/crud-pokemon/js/index.js
@@ -4,23 +4,6 @@ const documentReady = () => {
   const tBodyPokemons = document.getElementById("tBodyPokemons");
   const formPokemon = document.getElementById("formPokemon");
 
-  tBodyPokemons.innerHTML += `
-
-    <tr>
-        <td> 1 </td>
-        <td> Pikachu </td>
-        <td>Electrico</td>
-        <td>250 </td>
-        <td>Trueno</td>
-        <td>Impactrueno</td>
-        <td>
-            <img src="https://images.wikidexcdn.net/mwuploads/wikidex/thumb/7/77/latest/20150621181250/Pikachu.png/1200px-Pikachu.png"
-                alt="Pikachu" class="img-fluid" style="max-width: 128px;" />
-        </td>
-        <td> 🗑️ | ✏️</td>
-    </tr>
-  `;
-
   const createPokemon = (e) => {
     e.preventDefault();
     const documentFormPokemon = document.forms["formPokemon"];
@@ -31,6 +14,19 @@ const documentReady = () => {
     const special = documentFormPokemon.special.value;
     const imgUrl = documentFormPokemon.imgUrl.value;
     console.log({ name, type, hp, attack, special, imgUrl });
+
+    tBodyPokemons.innerHTML += `<tr>
+      <td> 1 </td>
+      <td> ${name} </td>
+      <td>${type}</td>
+      <td>${hp} </td>
+      <td>${attack}</td>
+      <td>${special}</td>
+      <td>
+      ${imgUrl}
+      </td>
+      <td> 🗑️ | ✏️</td>
+  </tr>`;
   };
 
   formPokemon.addEventListener("submit", createPokemon);
