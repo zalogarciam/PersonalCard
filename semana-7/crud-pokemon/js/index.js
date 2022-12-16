aa8477425ddb969649226ba055dfcd4e9fc30f23
zalogarciam <gegarciam95@gmail.com>
2022-12-16 14:29:26 -0500

    Refactoring methods

--- semana-7/crud-pokemon/js/index.js
+++ semana-7/crud-pokemon/js/index.js
@@ -1,43 +1,41 @@
 "use strict";
 
-const documentReady = () => {
-  const POKEMONS_CRUD_DATA = "pokemons-crud";
-  const pokemons = JSON.parse(localStorage.getItem(POKEMONS_CRUD_DATA)) ?? [];
-  const formPokemon = document.getElementById("formPokemon");
+const POKEMONS_CRUD_DATA = "pokemons-crud";
+const pokemons = JSON.parse(localStorage.getItem(POKEMONS_CRUD_DATA)) ?? [];
 
-  const createPokemon = (e) => {
-    e.preventDefault();
-    const documentFormPokemon = document.forms["formPokemon"];
-    const name = documentFormPokemon.name.value;
-    const type = documentFormPokemon.type.value;
-    const hp = documentFormPokemon.hp.value;
-    const attack = documentFormPokemon.attack.value;
-    const special = documentFormPokemon.special.value;
-    const imgUrl = documentFormPokemon.imgUrl.value;
-    pokemons.push({ name, type, hp, attack, special, imgUrl });
-    localStorage.setItem(POKEMONS_CRUD_DATA, JSON.stringify(pokemons));
-    readPokemons();
-  };
+const createPokemon = (e) => {
+  e.preventDefault();
+  const documentFormPokemon = document.forms["formPokemon"];
+  const name = documentFormPokemon.name.value;
+  const type = documentFormPokemon.type.value;
+  const hp = documentFormPokemon.hp.value;
+  const attack = documentFormPokemon.attack.value;
+  const special = documentFormPokemon.special.value;
+  const imgUrl = documentFormPokemon.imgUrl.value;
+  pokemons.push({ name, type, hp, attack, special, imgUrl });
+  localStorage.setItem(POKEMONS_CRUD_DATA, JSON.stringify(pokemons));
+  readPokemons();
+};
 
-  const readPokemons = () => {
-    const tBodyPokemons = document.getElementById("tBodyPokemons");
-    tBodyPokemons.innerHTML = "";
-    pokemons.forEach((element, index) => {
-      const { name, type, hp, attack, special, imgUrl } = element;
-      tBodyPokemons.innerHTML += `
-        <tr>
-          <td>${index + 1}</td>
-          <td>${name}</td>
-          <td>${type}</td>
-          <td>${hp}</td>
-          <td>${attack}</td>
-          <td>${special}</td>
-          <td>
-            <img
-              src="${imgUrl}"
-              alt="${name}" class="img-fluid" style="max-width: 128px;" />
-          </td>
-          <td>
+const readPokemons = () => {
+  const tBodyPokemons = document.getElementById("tBodyPokemons");
+  tBodyPokemons.innerHTML = "";
+  pokemons.forEach((element, index) => {
+    const { name, type, hp, attack, special, imgUrl } = element;
+    tBodyPokemons.innerHTML += `
+      <tr>
+        <td>${index + 1}</td>
+        <td>${name}</td>
+        <td>${type}</td>
+        <td>${hp}</td>
+        <td>${attack}</td>
+        <td>${special}</td>
+        <td>
+          <img
+            src="${imgUrl}"
+            alt="${name}" class="img-fluid" style="max-width: 128px;" />
+        </td>
+        <td>
           <button 
             class="btn btn-danger"
             onclick="deletePokemon(${index})"
@@ -45,17 +43,20 @@ const documentReady = () => {
             🗑
           </button>
         </td>
-        </tr>
-      `;
-    });
-  };
-
-  readPokemons();
-  formPokemon.addEventListener("submit", createPokemon);
+      </tr>
+    `;
+  });
 };
 
 const deletePokemon = (index) => {
   alert("deletePokemon: " + index);
+};
+
+const documentReady = () => {
+  const formPokemon = document.getElementById("formPokemon");
+
+  readPokemons();
+  formPokemon.addEventListener("submit", createPokemon);
 };
 
 document.addEventListener("DOMContentLoaded", documentReady);
