300a60889dbb137150f1d86dc8df85a67403eb3c
zalogarciam <gegarciam95@gmail.com>
2022-12-16 23:03:28 -0500

    Add swiper and modify

--- semana-7/chanchirata/js/index.js
+++ semana-7/chanchirata/js/index.js
@@ -1,21 +1,16 @@
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "vertical",
-  loop: true,
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
 
-  // If we need pagination
+new Swiper(".swiper", {
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+  speed: 500,
   pagination: {
     el: ".swiper-pagination",
   },
-
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
   },
 });
