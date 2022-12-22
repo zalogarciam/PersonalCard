5c2022b8a39f36f9ea623de06af8d91487606a9e
zalogarciam <gegarciam95@gmail.com>
2022-12-21 20:41:45 -0500

    Adding dynamic card

--- semana-8/chanchirata/js/modules/food.js
+++ semana-8/chanchirata/js/modules/food.js
@@ -1,5 +1,18 @@
 const foods = () => {
-  console.log("test");
+  const foodsFoods = document.getElementById("foodsFoods");
+  foodsFoods.innerHTML = `
+  <div class="col-md-4">
+  <div class="card bg-dark border border-2 border-light border-opacity-25 mx-auto"
+    style="width: min(100%, 18rem);">
+    <img src="https://wallpapercave.com/wp/Pzb3LkG.jpg" class="card-img-top" alt="Anime 1" width="256"
+      height="144" style="width: 100%; height: 192px; object-fit: cover;">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+        card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+  `;
 };
-
 export default foods;
