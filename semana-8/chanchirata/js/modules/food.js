a1e504d34e28f4e6773c90588e6210ba955ccfd6
zalogarciam <gegarciam95@gmail.com>
2022-12-21 22:58:49 -0500

    Update html

--- semana-8/chanchirata/js/modules/food.js
+++ semana-8/chanchirata/js/modules/food.js
@@ -1,29 +1,29 @@
 import { comidas } from "../../utils/data.js";
 
 const foods = () => {
-  const foodsFoods = document.getElementById("foodsFoods");
+  const foodsFoods = document.getElementById('foodsFoods');
   comidas.forEach((element) => {
     const { id, name, description, urlImage, urlRecipe } = element;
 
     foodsFoods.innerHTML += `
-        <div class="col-md-4" key=${id}>
-          <div class="card bg-dark border border-2 border-light border-opacity-25 mx-auto"
-            style="width: min(100%, 18rem);">
-            <div class="card-header">
-              ${name}
-            </div>
-            <div class="card-body">
-              <img src="${urlImage}" class="card-img-top" alt="Anime 1" width="256" height="144"
-                style="width: 100%; height: 192px; object-fit: cover;">
-              <h5 class="card-title">${name}</h5>
-              <p class="card-text">${description}</p>
-            </div>
-            <div class="card-footer text-muted">
-              <a href="${urlRecipe}" class="btn btn-primary">Receta</a>
-            </div>
+      <div class="col-md-4" key=${id}>
+        <div class="card bg-dark border border-2 border-light border-opacity-25 h-100 mx-auto"
+          style="width: min(100%, 18rem);">
+          <div class="card-header text-center">
+            ${name}
+          </div>
+          <div class="card-body">
+            <img src="${urlImage}" class="card-img-top" alt="Anime 1" width="256" height="144"
+              style="width: 100%; height: 192px; object-fit: cover; border-radius: 0.5rem">
+            <h5 class="card-title mt-2">${name}</h5>
+            <p class="card-text">${description}</p>
+          </div>
+          <div class="card-footer text-center">
+            <a href="${urlRecipe}" target="_blank" rel="noopener noreferrer" class="btn btn-primary">Receta</a>
           </div>
         </div>
-      `;
+      </div>
+    `;
   });
 };
 
