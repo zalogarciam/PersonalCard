efd80c8d01910249e0d0facc248e41f145073548
zalogarciam <gegarciam95@gmail.com>
2023-01-06 14:22:23 -0500

    Header component as example

--- semana-9/patita-feliz/src/App.js
+++ semana-9/patita-feliz/src/App.js
@@ -1,8 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/sections/Header";
 
 function App() {
-  return <h1>Patita Feliz</h1>;
+  return <Header />;
 }
 
 export default App;
---
+++ semana-9/patita-feliz/src/components/sections/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Patita Feliz</h1>
+    </header>
+  );
+};
+
+export default Header;
