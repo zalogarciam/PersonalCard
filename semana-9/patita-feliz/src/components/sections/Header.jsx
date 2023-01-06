5f8dc4c08ca191212db24021bffc9a6ced0806cc
zalogarciam <gegarciam95@gmail.com>
2023-01-06 14:54:00 -0500

    Adding some styles in header

--- semana-9/patita-feliz/src/components/sections/Header.jsx
+++ semana-9/patita-feliz/src/components/sections/Header.jsx
@@ -1,7 +1,12 @@
 const Header = () => {
   return (
-    <header className="container">
-      <h1>Patita Feliz</h1>
+    <header className="py-3">
+      <div className="container">
+        <h1 className="text-info text-center">Patita Feliz</h1>
+        <h2 className="text-primary fs-5 m-0 text-center">
+          La mejor veterinaria del Peru
+        </h2>
+      </div>
     </header>
   );
 };
