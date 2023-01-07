c3e8847cb104502ead6c5e6b1a5116ce224345cc
zalogarciam <gegarciam95@gmail.com>
2023-01-07 13:28:26 -0500

    Conditinal if no appointments

--- semana-9/patita-feliz/src/components/appointments/AppointmentsCards.jsx
+++ semana-9/patita-feliz/src/components/appointments/AppointmentsCards.jsx
@@ -5,9 +5,18 @@ const AppointmentsCards = ({ appointments }) => {
     <div className="col-md-6 d-flex flex-column gap-3">
       <h3 className="text-center m-0">🐱 Citas 🐱</h3>
       <ul className="list-group">
-        {appointments.map((element, index) => {
-          return <AppointmentsCard key={index} appointment={element} />;
-        })}
+        {appointments.length <= 0 ? (
+          <li
+            className="list-group-item list-group-item-action bg-transparent text-light text-center fw-bold"
+            aria-current="true"
+          >
+            No hay citas registradas
+          </li>
+        ) : (
+          appointments.map((element, index) => {
+            return <AppointmentsCard key={index} appointment={element} />;
+          })
+        )}
       </ul>
     </div>
   );
