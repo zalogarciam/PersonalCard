e2bfa736974fe2e95c7f9dabf72e6ec7081dd13a
zalogarciam <gegarciam95@gmail.com>
2023-01-20 14:39:02 -0500

    Install swiper

--- semana-11/eakon-frontend/src/components/home/HomeHero.jsx
+++ semana-11/eakon-frontend/src/components/home/HomeHero.jsx
@@ -1,5 +1,23 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
 const HomeHero = () => {
-    return ( <h1>Home Hero</h1> );
-}
- 
+  return (
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log("slide change")}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper>
+  );
+};
+
 export default HomeHero;
