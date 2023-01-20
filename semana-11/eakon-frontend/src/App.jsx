b8747ea7e3ec14dc4b2ceb76f11f236b7717838e
zalogarciam <gegarciam95@gmail.com>
2023-01-19 20:13:08 -0500

    Axios instance implementation

--- semana-11/eakon-frontend/src/App.jsx
+++ semana-11/eakon-frontend/src/App.jsx
@@ -3,9 +3,9 @@ import "./App.css";
 import { primaryRoute } from "./routes/primaryRoute";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
+import axiosInstance from "./configs/axiosInstance";
 
 function App() {
-  console.log(import.meta.env.VITE_EAKON_CMS_API_URL);
 
   return (
     <Provider store={store}>
