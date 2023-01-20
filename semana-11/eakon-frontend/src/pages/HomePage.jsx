189d29d54c31783aa8bd97451472511a76a662e4
zalogarciam <gegarciam95@gmail.com>
2023-01-20 14:25:12 -0500

    Adding custom hooks

--- semana-11/eakon-frontend/src/pages/HomePage.jsx
+++ semana-11/eakon-frontend/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import HomeHero from "../components/home/HomeHero";
+import useHome from "../hooks/useHome";
 import { fetchReadHeroProducts } from "../redux/thunks/homeThunk";
 
 const HomePage = () => {
   const dispatch = useDispatch();
+  const { heroProducts } = useHome();
 
   useEffect(() => {
     dispatch(fetchReadHeroProducts());
