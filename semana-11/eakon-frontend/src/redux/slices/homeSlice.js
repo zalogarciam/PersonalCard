5eeca0157fec37c2082ca984693660ef9af05ed6
zalogarciam <gegarciam95@gmail.com>
2023-01-19 21:02:17 -0500

    adding api call and slice

--- semana-11/eakon-frontend/src/redux/slices/homeSlice.js
+++ semana-11/eakon-frontend/src/redux/slices/homeSlice.js
@@ -1,15 +1,37 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchReadProducts } from "../../thunks/homeThunk";
 
 const initialState = {
   loading: false,
   error: {},
+  heroProducts: [],
 };
 
 export const homeSlice = createSlice({
   name: "home",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchReadProducts.pending, ({loading}) => {
+      loading = true;
+    });
+    builder.addCase(
+      fetchReadProducts.fulfilled,
+      ({ loading, error, heroProducts }, { payload }) => {
+        state.loading = false;
+        error = {};
+        heroProducts = payload;
+      }
+    );
+    builder.addCase(
+      fetchReadProducts.rejected,
+      ({ loading, error, heroProducts }, { payload }) => {
+        state.loading = false;
+        error = {};
+        heroProducts = [];
+      }
+    );
+  },
 });
-
 
 export default homeSlice.reducer;
--- semana-11/eakon-frontend/src/thunks/homeThunk.js
+++ semana-11/eakon-frontend/src/thunks/homeThunk.js
@@ -1,13 +1,22 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import axiosInstance from "../configs/axiosInstance";
 
 export const fetchReadProducts = createAsyncThunk(
-    'home/fetchReadProducts',
-    async (_, {rejectWithValue}) => {
-        try {
-            
-        } catch (error) {
-            
-        }
+  "home/fetchReadProducts",
+  async (_, { rejectWithValue }) => {
+    try {
+      const options = {
+        method: "GET",
+        url: `/productos?filters[posicion][slug][$eq]=cabecera&populate=subcategoria,marca,imagen`,
+      };
 
+      const { data } = await axiosInstance(options);
+      console.log(data.data);
+      console.log(data.data ?? "Chanchito");
+      return data.data ?? [];
+    } catch (error) {
+      console.log(error);
+      return rejectWithValue(error);
     }
+  }
 );
