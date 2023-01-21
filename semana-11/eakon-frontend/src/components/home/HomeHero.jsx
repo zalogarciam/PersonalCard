c9134ddb626594d3a0e330d707d4797106159571
zalogarciam <gegarciam95@gmail.com>
2023-01-20 21:37:26 -0500

    Adding card and container style

--- semana-11/eakon-frontend/src/components/home/HomeHero.jsx
+++ semana-11/eakon-frontend/src/components/home/HomeHero.jsx
@@ -22,23 +22,25 @@ const HomeHero = ({ heroProducts }) => {
             key={element.id}
             tag="section"
             zoom={true}
-            className="section hero"
+            className="section hero d-flex a-items-center"
             style={{
               // backgroundImage: `linear-gradient(to right, var(--light-primary-shadow-color), var(--light-secondary-shadow-color)), url(${imagen.data[0].attributes.url})`
               backgroundImage: `url(${imagen.data[0].attributes.url})`,
             }}
           >
-            <div className="container">
+            <div className="container container--hero">
               <div className="card card--hero">
-                <h2 className="card__title card__title--hero">{nombre}</h2>
-                <h3 className="card__subtitle card__subtitle--hero">
-                  {marca.data[0].attributes.nombre} I{" "}
-                  {subcategoria.data.attributes.nombre}
-                </h3>
-                {/* <Link to={`/productos/${slug}`} className="button button--primary">Ver más</Link> */}
-                <Link to={`/`} className="button button--primary">
-                  Ver más
-                </Link>
+                <div className="card__body card__body--hero">
+                  <h2 className="card__title card__title--hero">{nombre}</h2>
+                  <h3 className="card__subtitle card__subtitle--hero">
+                    {marca.data[0].attributes.nombre} I{" "}
+                    {subcategoria.data.attributes.nombre}
+                  </h3>
+                  {/* <Link to={`/productos/${slug}`} className="button button--primary">Ver más</Link> */}
+                  <Link to={`/`} className="button button--primary">
+                    Ver más
+                  </Link>
+                </div>
               </div>
             </div>
           </SwiperSlide>
