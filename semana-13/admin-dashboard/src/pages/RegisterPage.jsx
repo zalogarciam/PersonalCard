f0d8e5c142af50c3ea4fa8723c0a401b953d5d2d
zalogarciam <gegarciam95@gmail.com>
2023-02-03 15:57:05 -0500

    Add form for register page

--- semana-13/admin-dashboard/src/pages/RegisterPage.jsx
+++ semana-13/admin-dashboard/src/pages/RegisterPage.jsx
@@ -1,7 +1,50 @@
+import { useState } from "react";
+
 const RegisterPage = () => {
-    return (
-      <h1>RegisterPage</h1>
-    );
+  const [registrationForm, setRegistrationForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = registrationForm;
+
+  const handleChangeRegistrationForm = (e) => {
+    setRegistrationForm({
+      ...registrationForm,
+      [e.target.name]: e.target.value,
+    });
   };
-  
-  export default RegisterPage;
+
+  const handleSubmitCreateUser = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <h1>RegisterPage</h1>
+      <form onSubmit={handleSubmitCreateUser}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Correo"
+          value={email}
+          onChange={handleChangeRegistrationForm}
+        />
+        <br />
+        <br />
+        <input
+          type="password"
+          name="password"
+          placeholder="Contraseña"
+          value={password}
+          onChange={handleChangeRegistrationForm}
+        />
+        <br />
+        <br />
+        <input type="submit" value="Registrar" />
+      </form>
+    </>
+  );
+};
+
+export default RegisterPage;
